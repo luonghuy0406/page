643f232551774cc75ffcbe03cab31a39e13b38d3
luonghuy0406 <63404335+luonghuy0406@users.noreply.github.com>
2023-08-07 16:50:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+(function(d) {
+      var wf = d.createElement('script'), s = d.scripts[0];
+      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+      wf.async = true;
+      s.parentNode.insertBefore(wf, s);
+   })(document);
+
 var query = window.location.search.substring(1);
 function parse_query_string(query) {
   var vars = query.split("&");
